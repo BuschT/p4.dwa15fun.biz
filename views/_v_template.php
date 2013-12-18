033157666d6b6ae85c3c6ab729e66b3d30cee15e
Taylor Busch <Taylor.R.Busch.-ND@espn.com>
2013-12-17 19:22:38 -0500

    Setup basic page

--- views/_v_template.php
+++ views/_v_template.php
@@ -1,19 +1,63 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title><?php if(isset($title)) echo $title; ?></title>
+	<title>BlogTastic!</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
 
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />	
-					
+	<!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="/css/jumbotron-narrow.css" rel="stylesheet">
+
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
-	
+
 </head>
 
-<body>	
+<body>
+	<div class="container">
+	  <div class="header">
+		<ul class="nav nav-pills pull-right">
+		  <li><a href="/">Home</a></li>
+		  <!-- Menu for users who are logged in -->
+			<?php if($user): ?>
+				<li><a href='/users/logout'>Logout</a></li>
+			<!-- Menu options for users who are not logged in -->
+			<?php else: ?>
+				<li><a href='/users/signup'>Sign up</a></li>
+				<li><a href='/users/login'>Log in</a></li>
+			<?php endif; ?>
+		</ul>
+		<h3 class="text-muted">Quizes!!</h3>
+	  </div>
+	  <div class="content_options">
+	  	<ul class="nav nav-pills">
+	  		<li id="nav_posts"><a href="/posts">Take Quiz</a></li>
+	  		<li id="nav_posts_add"><a href="/posts/add">Add Quiz</a></li>
+	  		<li id="nav_posts_users"><a href="/posts/users">Get Score</a></li>
+	  	</ul>
+	  </div>
+	  <div class="jumbotron">
+		 <!-- Content! -->
+		 <?php if(isset($content)) echo $content; ?>
+		 <!-- Error section if need be -->
+		 <div class="error"></div>
+	  </div>
 
-	<?php if(isset($content)) echo $content; ?>
+	  <div class="footer">
+		<p>&copy; QuizTastic! 2013</p>
+	  </div>
+
+    </div> <!-- /container -->
+
+    <br>
 
 	<?php if(isset($client_files_body)) echo $client_files_body; ?>
+
+	<!-- placed at end of file so code page loads faster -->
+	<script src="https://code.jquery.com/jquery.js"></script>
+	<script src="/js/bootstrap.min.js"></script>
+
 </body>
 </html>
--- views/v_index_index.php
+++ views/v_index_index.php
@@ -1,11 +1,5 @@
 <p>
-	Hello World! You have successfully spawned a new application.
-</p>
-
-<p>
-	This message is being triggered via the c_index.php controller, within the index() method.
-</p>
-
-<p>
-	<strong>Since everything is in working order, you should now delete <?php echo APP_PATH?>diagnostics.php</strong>
+	Welcome To The Online Quiz Thingy<br /><br />
+	This page probably looks best in Chrome, which is what I mainly designed it for.
+	<br /><br />
 </p>
