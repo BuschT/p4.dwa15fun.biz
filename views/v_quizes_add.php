6c31a37dc8fb422efd30f565a7b91ad1c0461e3c
Taylor Busch <Taylor.R.Busch.-ND@espn.com>
2013-12-20 12:44:09 -0500

    validation fixes

--- views/v_quizes_add.php
+++ views/v_quizes_add.php
@@ -2,18 +2,18 @@
 <form method='POST' action='/quizes/p_add' id="form_newquiz">
 	<label for='newquiz_name'>Quiz Name:</label>
 	<input name='newquiz_name' maxlength='100' id='newquiz_name' class='quizinput'/><br />
-	<label for='newquiz_title'>Quiz Description:</label>
+	<label for='newquiz_description'>Quiz Description:</label>
 	<input name='newquiz_description' maxlength='100' id='newquiz_description' class='quizinput'/><br /><br />
 
-	<label for='newquiz_content'>Question #1:</label>
+	<label for='newquiz_question1'>Question #1:</label>
 	<input name='newquiz_question1' maxlength='100' id='newquiz_question1' class='quizinput'/>
-	<label for='newquiz_content'>Answer A:</label>
+	<label for='newquiz_question1_answerA'>Answer A:</label>
 	<input name='newquiz_question1_answerA' maxlength='50' id='newquiz_question1A' class='quizinput'/>
-	<label for='newquiz_content'>Answer B:</label>
+	<label for='newquiz_question1_answerB'>Answer B:</label>
 	<input name='newquiz_question1_answerB' maxlength='50' id='newquiz_question1B' class='quizinput'/>
-	<label for='newquiz_content'>Answer C:</label>
+	<label for='newquiz_question1_answerC'>Answer C:</label>
 	<input name='newquiz_question1_answerC' maxlength='50' id='newquiz_question1C' class='quizinput'/>
-	<label for='newquiz_content'>Answer D:</label>
+	<label for='newquiz_question1_answerD'>Answer D:</label>
 	<input name='newquiz_question1_answerD' maxlength='50' id='newquiz_question1D' class='quizinput'/>
 	<br /><br />
 	<label for='correct_answer1'>Correct Answer:</label>
@@ -23,18 +23,18 @@
 	D<input type="radio" name="correct_answer1" value="D">
 	<br />
 	<br />
-	<label for='newquiz_content'>Question #2:</label>
+	<label for='newquiz_question2'>Question #2:</label>
 	<input name='newquiz_question2' maxlength='100' id='newquiz_question2' class='quizinput'/>
-	<label for='newquiz_content'>Answer A:</label>
+	<label for='newquiz_question2_answerA'>Answer A:</label>
 	<input name='newquiz_question2_answerA' maxlength='50' id='newquiz_question2A' class='quizinput'/>
-	<label for='newquiz_content'>Answer B:</label>
+	<label for='newquiz_question2_answerB'>Answer B:</label>
 	<input name='newquiz_question2_answerB' maxlength='50' id='newquiz_question2B' class='quizinput'/>
-	<label for='newquiz_content'>Answer C:</label>
+	<label for='newquiz_question2_answerC'>Answer C:</label>
 	<input name='newquiz_question2_answerC' maxlength='50' id='newquiz_question2C' class='quizinput'/>
-	<label for='newquiz_content'>Answer D:</label>
+	<label for='newquiz_question2_answerD'>Answer D:</label>
 	<input name='newquiz_question2_answerD' maxlength='50' id='newquiz_question2D' class='quizinput'/>
 	<br /><br />
-	<label for='correct_answer1'>Correct Answer:</label>
+	<label for='correct_answer2'>Correct Answer:</label>
 	A<input type="radio" name="correct_answer2" value="A" checked>
 	B<input type="radio" name="correct_answer2" value="B">
 	C<input type="radio" name="correct_answer2" value="C">
@@ -42,54 +42,54 @@
 	<br>
 	<br />
 	<br />
-	<label for='newquiz_content'>Question #3:</label>
+	<label for='newquiz_question3'>Question #3:</label>
 	<input name='newquiz_question3' maxlength='100' id='newquiz_question3' class='quizinput'/>
-	<label for='newquiz_content'>Answer A:</label>
+	<label for='newquiz_question3_answerA'>Answer A:</label>
 	<input name='newquiz_question3_answerA' maxlength='50' id='newquiz_question3A' class='quizinput'/>
-	<label for='newquiz_content'>Answer B:</label>
+	<label for='newquiz_question3_answerB'>Answer B:</label>
 	<input name='newquiz_question3_answerB' maxlength='50' id='newquiz_question3B' class='quizinput'/>
-	<label for='newquiz_content'>Answer C:</label>
+	<label for='newquiz_question3_answerC'>Answer C:</label>
 	<input name='newquiz_question3_answerC' maxlength='50' id='newquiz_question3C' class='quizinput'/>
-	<label for='newquiz_content'>Answer D:</label>
+	<label for='newquiz_question3_answerD'>Answer D:</label>
 	<input name='newquiz_question3_answerD' maxlength='50' id='newquiz_question3D' class='quizinput'/>
 	<br /><br />
-	<label for='correct_answer1'>Correct Answer:</label>
+	<label for='correct_answer3'>Correct Answer:</label>
 	A<input type="radio" name="correct_answer3" value="A" checked>
 	B<input type="radio" name="correct_answer3" value="B">
 	C<input type="radio" name="correct_answer3" value="C">
 	D<input type="radio" name="correct_answer3" value="D">
 	<br>
 	<br />
-	<label for='newquiz_content'>Question #4:</label>
+	<label for='newquiz_question4'>Question #4:</label>
 	<input name='newquiz_question4' maxlength='100' id='newquiz_question4' class='quizinput'/>
-	<label for='newquiz_content'>Answer A:</label>
+	<label for='newquiz_question4_answerA'>Answer A:</label>
 	<input name='newquiz_question4_answerA' maxlength='50' id='newquiz_question4A' class='quizinput'/>
-	<label for='newquiz_content'>Answer B:</label>
+	<label for='newquiz_question4_answerB'>Answer B:</label>
 	<input name='newquiz_question4_answerB' maxlength='50' id='newquiz_question4B' class='quizinput'/>
-	<label for='newquiz_content'>Answer C:</label>
+	<label for='newquiz_question4_answerC'>Answer C:</label>
 	<input name='newquiz_question4_answerC' maxlength='50' id='newquiz_question4C' class='quizinput'/>
-	<label for='newquiz_content'>Answer D:</label>
+	<label for='newquiz_question4_answerD'>Answer D:</label>
 	<input name='newquiz_question4_answerD' maxlength='50' id='newquiz_question4D' class='quizinput'/>
 	<br /><br />
-	<label for='correct_answer1'>Correct Answer:</label>
+	<label for='correct_answer4'>Correct Answer:</label>
 	A<input type="radio" name="correct_answer4" value="A" checked>
 	B<input type="radio" name="correct_answer4" value="B">
 	C<input type="radio" name="correct_answer4" value="C">
 	D<input type="radio" name="correct_answer4" value="D">
 	<br />
 	<br />
-	<label for='newquiz_content'>Question #5:</label>
+	<label for='newquiz_question5'>Question #5:</label>
 	<input name='newquiz_question5' maxlength='100' id='newquiz_question5' class='quizinput'/>
-	<label for='newquiz_content'>Answer A:</label>
+	<label for='newquiz_question5_answerA'>Answer A:</label>
 	<input name='newquiz_question5_answerA' maxlength='50' id='newquiz_question5A' class='quizinput'/>
-	<label for='newquiz_content'>Answer B:</label>
+	<label for='newquiz_question5_answeB'>Answer B:</label>
 	<input name='newquiz_question5_answerB' maxlength='50' id='newquiz_question5B' class='quizinput'/>
-	<label for='newquiz_content'>Answer C:</label>
+	<label for='newquiz_question5_answerC'>Answer C:</label>
 	<input name='newquiz_question5_answerC' maxlength='50' id='newquiz_question5C' class='quizinput'/>
-	<label for='newquiz_content'>Answer D:</label>
+	<label for='newquiz_question5_answerD'>Answer D:</label>
 	<input name='newquiz_question5_answerD' maxlength='50' id='newquiz_question5D' class='quizinput'/>
 	<br /><br />
-    <label for='correct_answer1'>Correct Answer:</label>
+    <label for='correct_answer5'>Correct Answer:</label>
 	A<input type="radio" name="correct_answer5" value="A" checked>
 	B<input type="radio" name="correct_answer5" value="B">
 	C<input type="radio" name="correct_answer5" value="C">
