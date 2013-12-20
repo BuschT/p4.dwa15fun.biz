a7114f05fab1f776763a825bf217482a8da3f68c
Taylor Busch <Taylor.R.Busch.-ND@espn.com>
2013-12-19 22:39:56 -0500

    update DB name for live server

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'webmaster@myapp.com');
 
 # Default DB name for this app
-	define('DB_NAME', 'p4_dwa15fun_biz');
+	define('DB_NAME', 'dwafunbi_p4_dwa15fun_biz');
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
